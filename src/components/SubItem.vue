7f713abdf09444383326c5ff5afa81ff24c4fea3
jolker155 <tuananh.ln155@gmail.com>
2023-06-13 23:38:33 +0700

    update style

--- src/components/SubItem.vue
+++ src/components/SubItem.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
 div
   .flex.items-center 
-    el-checkbox(v-model='item.status')
-    .text.ml-2(:class="{ 'line-through': item.status }") {{ item.name }} 
+    .container-checkbox
+      input(type='checkbox' v-model='item.status')
+      span.checkmark-checkbox
+      label(:class="{ 'line-through': item.status }") {{ item.name }}
     .ml-auto.cursor-pointer(@click='remove(item.id)')
       svg.w-6.h-6(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor')
-        path(stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0')
+        path(stroke-linecap='round' stroke-linejoin='round' d='M6 18L18 6M6 6l12 12')
 
 </template>
 
@@ -40,4 +42,62 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.container-checkbox {
+  display: block;
+  position: relative;
+  font-weight: 500;
+  font-size: 14px;
+  color: #7e838f;
+  padding-left: 30px;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    z-index: 9;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  .checkmark-checkbox {
+    position: absolute;
+    top: 3px;
+    left: 0;
+    height: 18px;
+    width: 18px;
+    border: 1px solid #cdd0dc;
+    border-radius: 3px;
+    &:after {
+      content: "";
+      position: absolute;
+      display: none;
+      left: 3px;
+      top: 3px;
+      width: 10px;
+      height: 10px;
+      background-color: grey;
+    }
+  }
+}
+
+.container-checkbox input:checked ~ .checkmark-checkbox {
+  background-color: white;
+  border-color: #41454f;
+}
+
+.container-checkbox input:checked ~ label {
+  color: #41454f;
+}
+
+.container-checkbox input:checked ~ .checkmark-checkbox:after {
+  display: block;
+}
+</style>
